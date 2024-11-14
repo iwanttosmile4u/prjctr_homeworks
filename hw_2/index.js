4e822c100bd62152b8fae90575bf1744c761c7da
t.panasiuk <t.panasiuk@favbet.tech>
2024-11-14 23:22:39 +0200

    added one more case to the task 2, refactored code

--- hw_2/index.js
+++ hw_2/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-//Task1
+//Task 1
 
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
@@ -14,12 +14,43 @@ for (let i = 1; i <= 100; i++) {
   }
 }
 
-//Task2
+//Task 2
 
-const value = +prompt('Enter your number: ');
-const valueIsNumber = typeof value === 'number' && !isNaN(value);  
+//Case 1.
 
-// Using for
+const userValue = +prompt('Enter your number:'); 
+
+//Using for loop
+
+if (userValue) {
+  for (let i = 2; i < userValue; i+=2) {
+      console.log(i);
+  }
+} else {
+  console.log('Таке чуство шо Бог десь наказує нас за шось');
+}
+
+//Using while loop
+
+if (userValue) {
+  let counter = 2;
+
+  while(counter < userValue) {
+    console.log(counter);
+    counter+=2;
+  }
+} else {
+  console.log('Таке чуство шо Бог десь наказує нас за шось');
+}
+
+//Case 2. 
+//If we save own value in variable and don't use prompt, we need to check if this value is a number.
+//In this case my answer will be next:
+ 
+const value = 12; 
+const valueIsNumber = typeof value === 'number' && !isNaN(value);
+
+//Using for loop
 
 if (valueIsNumber) {
   for (let i = 2; i < value; i+=2) {
@@ -29,11 +60,15 @@ if (valueIsNumber) {
   console.log('Таке чуство шо Бог десь наказує нас за шось');
 }
 
-// Using while
+//Using while loop
 
-let counter = 2;
+if (valueIsNumber) {
+  let counter = 2;
 
-while(valueIsNumber && counter < value) {
-  console.log(counter);
-  counter+=2;
+  while(counter < value) {
+    console.log(counter);
+    counter+=2;
+  }
+} else {
+  console.log('Таке чуство шо Бог десь наказує нас за шось');
 }
